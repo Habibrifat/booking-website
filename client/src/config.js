ce1b18214b19063f6db22fb808a214df92ae02c5
Habibrifat <shah.newaz.habib.rifat@gmail.com>
2022-08-19 00:56:04 +0600

    fived commit

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-export  const  axiosInstance = axios.create({
+const  axiosInstance = axios.create({
     baseURL : "https://hotel-booking00.herokuapp.com",
 })
+
+export default axiosInstance;
