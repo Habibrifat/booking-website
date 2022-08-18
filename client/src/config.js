65c2a338059e0f3312d6243a054bbbeae6e37b6a
Habibrifat <shah.newaz.habib.rifat@gmail.com>
2022-08-19 01:28:25 +0600

    seven commit

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const  axiosInstance = axios.create({
-    baseURL : "https://hotel-booking00.herokuapp.com",
+    baseURL : "https://hotel-booking00.herokuapp.com/",
 })
 
 export default axiosInstance;
